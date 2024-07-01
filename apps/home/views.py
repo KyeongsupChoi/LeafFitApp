0d5993b37c8149d9df986e9007730e5fb6c73f80
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2024-07-01 10:54:50 +0900

    pdf and word export function

--- apps/home/views.py
+++ apps/home/views.py
@@ -11,6 +11,27 @@ from django.template import loader
 from django.urls import reverse
 from .forms import WendlerForm
 
+from django import template
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, FileResponse
+from django.shortcuts import render
+from django.template import loader
+from django.urls import reverse
+from docx import settings
+
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import Table, TableStyle, Paragraph
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.styles import getSampleStyleSheet
+
+from docx import Document
+from docx.shared import Inches
+
+from .forms import WendlerForm
+from reportlab.pdfgen import canvas
+import io
+
 
 @login_required(login_url="/login/")
 def index(request):
@@ -90,6 +111,88 @@ def wendler_view(request):
         form = WendlerForm()
 
     return render(request, 'home/wendler.html', {'form': form})
+
+def some_view(request):
+
+    styles = getSampleStyleSheet()
+    style = styles["BodyText"]
+
+    buffer = io.BytesIO()
+
+    canv = canvas.Canvas(buffer)
+
+    empty_list = []
+
+    empty_list.append(['Week No.', 'Set 1', "Set 2", "Set 3", "Set 4"])
+    empty_list.append(['Week 1'] + list(global_wendler_list['Week 1'].values()))
+    empty_list.append(['Week 2'] + list(global_wendler_list['Week 2'].values()))
+    empty_list.append(['Week 3'] + list(global_wendler_list['Week 3'].values()))
+    empty_list.append(['Week 4'] + list(global_wendler_list['Week 4'].values()))
+
+    print(empty_list)
+
+    header = Paragraph("<bold><font size=18>Wendler Exercise List</font></bold>", style)
+
+    data = empty_list
+    t = Table(data)
+    t.setStyle(TableStyle([("BOX", (0, 0), (-1, -1), 0.25, colors.black),
+                           ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black)]))
+    data_len = len(data)
+
+    for each in range(data_len):
+        if each % 2 == 0:
+            bg_color = colors.whitesmoke
+        else:
+            bg_color = colors.lightgrey
+
+        t.setStyle(TableStyle([('BACKGROUND', (0, each), (-1, each), bg_color)]))
+
+    aW = 540
+    aH = 720
+
+    w, h = header.wrap(aW, aH)
+    header.drawOn(canv, 72, 800)
+    aH = aH - h
+    w, h = t.wrap(aW, aH)
+    t.drawOn(canv, 72, aH - h)
+    canv.save()
+
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='WendlerSheet.pdf')
+
+def word_doc_view(request):
+
+    document = Document()
+    docx_title="WendlerSheet.docx"
+
+    document.add_paragraph("Wendler Exercise List")
+
+    document.add_paragraph('Week 1' + str(global_wendler_list['Week 1'])[1:-1])
+
+    document.add_paragraph('Week 2' + str(global_wendler_list['Week 2'])[1:-1])
+
+    document.add_paragraph('Week 3' + str(global_wendler_list['Week 3'])[1:-1])
+
+    document.add_paragraph('Week 4' + str(global_wendler_list['Week 4'])[1:-1])
+
+    document.add_page_break()
+
+    # Prepare document for download
+    # -----------------------------
+    f = io.BytesIO()
+    document.save(f)
+    length = f.tell()
+    f.seek(0)
+    response = HttpResponse(
+        f.getvalue(),
+        content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    )
+    response['Content-Disposition'] = 'attachment; filename=' + docx_title
+    response['Content-Length'] = length
+    return response
 
 
 @login_required(login_url="/login/")
